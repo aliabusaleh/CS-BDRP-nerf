2f5262c12b39a0fc933cf66fffe7418655e1a9ab
SYusupov <sayor1999@gmail.com>
2024-01-14 15:16:52 +0100

    Adding concurrency for faster execution
    Still takes 0.65 seconds per image

--- noiseDegradation/apply_nds.py
+++ noiseDegradation/apply_nds.py
@@ -1,6 +1,7 @@
 import cv2
 import torchvision
 import torch
+import concurrent.futures
 
 from vimeo_7f import aug_moving
 from nds_degradation import *
@@ -8,40 +9,58 @@ from nds_degradation import *
 import os
 import argparse
 
-def apply_nds(img_directory, output_directory):
+def run_for_dir(img_directory, output_directory):
     """Apply NeRF-Style Degradation Simulator (NDS)
         adapted from https://github.com/redrock303/NeRFLiX_CVPR2023
         - img_directory - directory where the original images are stored
         - output_directory - directory where to store the noisy images"""
-    
-    for filename in os.listdir(img_directory):
-        if filename.endswith(".png"):
-            img_path = os.path.join(img_directory, filename)
-            output_path = os.path.join(output_directory, filename)
 
-            img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:  # Use ThreadPoolExecutor for I/O-bound tasks
+        futures = []
+        for i, filename in enumerate(os.listdir(img_directory)):
+            if filename.endswith(".png"):
+                img_path = os.path.join(img_directory, filename)
+                output_path = os.path.join(output_directory, filename)
 
-            patch_width=196
-            patch_height=196
+                if not os.path.exists(output_path):
+                    future = executor.submit(apply_nds, img_path, output_path)
+                    futures.append(future)
+                # else:
+                #     print("Already")
+        
+        concurrent.futures.wait(futures)
+            
+        # if i%100 == 0:
+        #     print(f"Processed {i} images")
+        
+        # if i > 5:
+        #     break
 
-            H,W, c = img.shape
-            coord_map_dict ={'{}-{}'.format(patch_height,patch_width):defineCoorMap(patch_height,patch_width)}
-            key = '{}-{}'.format(H,W)
-            if key in coord_map_dict:
-                coord_map = coord_map_dict[key]
-            else :
-                coord_map_dict[key] = defineCoorMap(H,W)
-                coord_map = coord_map_dict[key]
-            mask = defineHighlightArea(H,W,coord_map.copy())
-            tmp = color_jet(img)
-            # img = img * (1-mask) + mask * tmp
+def apply_nds(img_path, output_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            img,jpeg_quality,noise_level = process(img,coord_map.copy())
-            img = reposition(img,ratio=0.3)
-            img = img.transpose(2,0,1)
-            lr_tensor = torch.from_numpy(img.astype(np.float32) / 255.0).float()
-            torchvision.utils.save_image(lr_tensor,output_path)
+    patch_width=196
+    patch_height=196
+
+    H,W, c = img.shape
+    coord_map_dict ={'{}-{}'.format(patch_height,patch_width):defineCoorMap(patch_height,patch_width)}
+    key = '{}-{}'.format(H,W)
+    if key in coord_map_dict:
+        coord_map = coord_map_dict[key]
+    else :
+        coord_map_dict[key] = defineCoorMap(H,W)
+        coord_map = coord_map_dict[key]
+    mask = defineHighlightArea(H,W,coord_map.copy())
+    tmp = color_jet(img)
+    # img = img * (1-mask) + mask * tmp
+
+    img,jpeg_quality,noise_level = process(img,coord_map.copy())
+    img = reposition(img,ratio=0.3)
+    img = img.transpose(2,0,1)
+    lr_tensor = torch.from_numpy(img.astype(np.float32) / 255.0).float()
+    torchvision.utils.save_image(lr_tensor,output_path)
+    # print("Done")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -52,4 +71,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     arguments = vars(args)
-    apply_nds(args['img_dir'], args['out_dir'])
+    run_for_dir(arguments['img_dir'], arguments['out_dir'])
